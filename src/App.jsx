991d2a355ecbc58cbece0f2cf986c728817fa605
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-23 14:27:48 +0200

    new page CycleOne

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import BannerFive from "./components/BannerFive";
-import BannerFour from "./components/BannerFour";
-import Bannerthree from "./components/BannerThree";
-import Bannertwo from "./components/BannerTwo";
-import BannerOne from "./components/BannerOne";
-import BannerSeven from "./components/BannerSeven";
-import BannerEight from "./components/BannerEight";
+import CycleOne from "./pages/CycleOne";
+
 function App() {
   return (
     <>
-      <BannerFive />
-      <BannerFour />
-      <Bannerthree />
-      <Bannertwo />
-      <BannerOne />
-      <BannerSeven />
-      <BannerEight />
+      <CycleOne />
     </>
   );
 }
---
+++ src/pages/CycleOne.jsx
@@ -0,0 +1,22 @@
+import BannerFive from "../components/BannerFive";
+import BannerFour from "../components/BannerFour";
+import Bannerthree from "../components/BannerThree";
+import Bannertwo from "../components/BannerTwo";
+import BannerOne from "../components/BannerOne";
+import BannerSeven from "../components/BannerSeven";
+import BannerEight from "../components/BannerEight";
+const CycleOne = () => {
+  return (
+    <div>
+      <BannerFive />
+      <BannerFour />
+      <Bannerthree />
+      <Bannertwo />
+      <BannerOne />
+      <BannerSeven />
+      <BannerEight />
+    </div>
+  );
+};
+
+export default CycleOne;
