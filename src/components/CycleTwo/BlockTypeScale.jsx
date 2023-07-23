961bf1a3ad668bba1a197d023935f90e19853595
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-23 20:34:12 +0200

    cycletwo page style

--- src/components/CycleTwo/BlockTypeScale.jsx
+++ src/components/CycleTwo/BlockTypeScale.jsx
@@ -1,13 +1,31 @@
 import { useState, useEffect, useRef } from "react";
 import capitalsUTC from "../../data/citiesData";
 
-const BlockType = () => {
+const BlockTypeScale = () => {
   const [randomIndex, setRandomIndex] = useState(null);
   const blockTypeBottomRef = useRef(null);
 
   useEffect(() => {
     getRandomIndex();
   }, []);
+
+  //   useEffect(() => {
+  //     const delay = setTimeout(() => {
+  //       if (blockTypeBottomRef.current) {
+  //         gsap.to(blockTypeBottomRef.current, {
+  //           duration: 1,
+  //           scaleX: 2,
+  //           scaleY: 2,
+  //           y: "100%",
+  //           transformOrigin: "0% 50%", // Set the transform origin to keep the left edge fixed
+
+  //           ease: "power3.inOut",
+  //         });
+  //       }
+  //     }, 1000);
+
+  //     return () => clearTimeout(delay);
+  //   }, []);
 
   const getRandomIndex = () => {
     const newIndex = Math.floor(Math.random() * capitalsUTC.length);
@@ -33,4 +51,4 @@ const BlockType = () => {
   );
 };
 
-export default BlockType;
+export default BlockTypeScale;
--- src/pages/CycleTwo.jsx
+++ src/pages/CycleTwo.jsx
@@ -1,9 +1,17 @@
+import BlockTypeScale from "../components/CycleTwo/BlockTypeScale";
 import BlockType from "../components/CycleTwo/BlockType";
 
 const CycleTwo = () => {
   return (
-    <div>
-      <BlockType />
+    <div className="cycletwo">
+      <div className="cycletwo__top">
+        <BlockTypeScale />
+        <BlockType />
+      </div>
+      <div className="cycletwo__bottom">
+        <BlockType />
+        <BlockType />
+      </div>
     </div>
   );
 };
