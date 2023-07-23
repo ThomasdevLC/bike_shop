49ce339a17f1c1c8008d26d324fd48ed97b69a06
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-23 15:57:09 +0200

    cycletwo effect scale

--- src/components/CycleTwo/BlockType.jsx
+++ src/components/CycleTwo/BlockType.jsx
@@ -1,13 +1,53 @@
+import { useState, useEffect, useRef } from "react";
+import capitalsUTC from "../../data/citiesData";
+
 const BlockType = () => {
+  const [randomIndex, setRandomIndex] = useState(null);
+  const blockTypeBottomRef = useRef(null);
+
+  useEffect(() => {
+    getRandomIndex();
+  }, []);
+
+  useEffect(() => {
+    // Trigger the animation after a delay of 1 second
+    const delay = setTimeout(() => {
+      if (blockTypeBottomRef.current) {
+        gsap.to(blockTypeBottomRef.current, {
+          duration: 0.8,
+          scaleX: 2,
+          scaleY: 2,
+          y: "100%",
+          transformOrigin: "0% 50%", // Set the transform origin to keep the left edge fixed
+
+          ease: "power3.inOut",
+        });
+      }
+    }, 1000);
+
+    // Clear the timeout when the component is unmounted or when randomIndex changes
+    return () => clearTimeout(delay);
+  }, []);
+
+  const getRandomIndex = () => {
+    const newIndex = Math.floor(Math.random() * capitalsUTC.length);
+    setRandomIndex(newIndex);
+  };
+
   return (
     <div className="blocktype">
       <div className="blocktype__top">
-        <p className="blocktype__top__name"></p>
-        <p className="blocktype__top__catch"></p>
+        <p className="blocktype__top__name">BIKKEY</p>
+        <p className="blocktype__top__catch">RIDE THE WORLD</p>
       </div>
-      <div className="blocktype__bottom">
-        <p className="blocktype__bottom__time"></p>
-        <p className="blocktype__bottom__cityone"></p>
+      <div ref={blockTypeBottomRef} className="blocktype__bottom">
+        <p className="blocktype__bottom__time">{`UTC ${capitalsUTC[randomIndex]?.utc}`}</p>
+        <p className="blocktype__bottom__cityone">
+          {capitalsUTC[randomIndex]?.city}
+        </p>
+        <p className="blocktype__bottom__citytwo">
+          {capitalsUTC[randomIndex + 1]?.city}
+        </p>
       </div>
     </div>
   );
---
+++ src/data/citiesData.js
@@ -0,0 +1,23 @@
+const capitalsUTC = [
+  { city: "PARIS", utc: "+00:00" },
+  { city: "LONDON", utc: "+01:00" },
+  { city: "BERLIN", utc: "+01:00" },
+  { city: "ROME", utc: "+02:00" },
+  { city: "MOSCOW", utc: "+03:00" },
+  { city: "BEIJING", utc: "+08:00" },
+  { city: "TOKYO", utc: "+09:00" },
+  { city: "NEW YORK", utc: "-04:00" },
+  { city: "LOS ANGELES", utc: "-07:00" },
+  { city: "SYDNEY", utc: "+10:00" },
+  { city: "DELHI", utc: "+05:30" },
+  { city: "BRASILIA", utc: "-03:00" },
+  { city: "CAIRO", utc: "+02:00" },
+  { city: "OTTAWA", utc: "-04:00" },
+  { city: "PRETORIA", utc: "+02:00" },
+  { city: "WELLINGTON", utc: "+12:00" },
+  { city: "JAKARTA", utc: "+07:00" },
+  { city: "ISLAMABAD", utc: "+05:00" },
+  { city: "BUENOS AIRES", utc: "-03:00" },
+];
+
+export default capitalsUTC;
