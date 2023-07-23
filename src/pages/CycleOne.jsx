aa209bb357b8f43eda8f7f4623a4671d926be64e
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-23 14:30:41 +0200

    new components folder cycleone

--- src/pages/CycleOne.jsx
+++ src/pages/CycleOne.jsx
@@ -1,10 +1,10 @@
-import BannerFive from "../components/BannerFive";
-import BannerFour from "../components/BannerFour";
-import Bannerthree from "../components/BannerThree";
-import Bannertwo from "../components/BannerTwo";
-import BannerOne from "../components/BannerOne";
-import BannerSeven from "../components/BannerSeven";
-import BannerEight from "../components/BannerEight";
+import BannerFive from "../components/CycleOne/BannerFive";
+import BannerFour from "../components/CycleOne/BannerFour";
+import Bannerthree from "../components/CycleOne/BannerThree";
+import Bannertwo from "../components/CycleOne/BannerTwo";
+import BannerOne from "../components/CycleOne/BannerOne";
+import BannerSeven from "../components/CycleOne/BannerSeven";
+import BannerEight from "../components/CycleOne/BannerEight";
 const CycleOne = () => {
   return (
     <div>
