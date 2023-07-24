586a740e123f09fa15a45b00af8b2316161ea77b
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-24 14:19:29 +0200

    cycletwo   overflow: hidden

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import CycleOne from "./pages/CycleOne";
-// import CycleTwo from "./pages/CycleTwo";
+// import CycleOne from "./pages/CycleOne";
+import CycleTwo from "./pages/CycleTwo";
 
 function App() {
   return (
     <>
-      <CycleOne />
-      {/* <CycleTwo /> */}
+      {/* <CycleOne /> */}
+      <CycleTwo />
     </>
   );
 }
