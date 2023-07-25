89981c9f22131be1753398f8e736b022c4b36007
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-25 13:50:34 +0200

    background color test

--- src/App.jsx
+++ src/App.jsx
@@ -43,9 +43,9 @@ function App() {
 
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+      initial={{ opacity: 0, y: "-100%" }}
+      animate={{ opacity: 1, y: "0%" }}
+      exit={{ opacity: 0, y: "100%" }}
       transition={{ duration: 0.5 }}
     >
       {pageToRender}
