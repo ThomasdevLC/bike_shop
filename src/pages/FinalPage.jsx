d016734ae88e7b3f296880ea36d41d4a98a7eab0
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-25 11:10:23 +0200

    finalPage style

--- src/pages/FinalPage.jsx
+++ src/pages/FinalPage.jsx
@@ -1,8 +1,11 @@
 const FinalPage = () => {
   return (
     <div className="finalpage">
-      <div className="finalpage__shop">shop</div>
-      <div className="finalpage__brand">ASPHALT</div>
+      <div className="finalpage__container">
+        <div className="finalpage__container__shop">shop</div>
+        <div className="finalpage__container__brand">ASPHALT</div>
+        <div className="finalpage__container__time">sat 29 july</div>
+      </div>
     </div>
   );
 };
