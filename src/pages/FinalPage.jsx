f26ecaeeb43b0748e00724b90834f7587bb2c70c
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-25 11:38:48 +0200

    finalpage anim

--- src/pages/FinalPage.jsx
+++ src/pages/FinalPage.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useRef } from "react";
+import SplitType from "split-type";
+
 const FinalPage = () => {
+  const splitBrandRef = useRef(null); // Utilisez un nom de variable différent pour useRef.
+
+  useEffect(() => {
+    if (splitBrandRef.current) {
+      const splitBrand = SplitType.create(splitBrandRef.current);
+      const sChar = splitBrand.chars[1];
+      const pChar = splitBrand.chars[2];
+      gsap.fromTo(
+        [sChar, pChar],
+        { y: "30%" },
+        { y: 0, stagger: 0.2, duration: 1.2, ease: "sine.inOut" }
+      );
+    }
+  }, []);
   return (
     <div className="finalpage">
       <div className="finalpage__container">
         <div className="finalpage__container__shop">shop</div>
-        <div className="finalpage__container__brand">ASPHALT</div>
+        <div className="finalpage__container__hidden">
+          <div
+            className="finalpage__container__hidden__brand"
+            ref={splitBrandRef}
+          >
+            {" "}
+            ASPHALT
+          </div>
+        </div>
         <div className="finalpage__container__time">sat 29 july</div>
       </div>
     </div>
