1f52a22e8e7ff0c4f2183db001e1729d88cc8a13
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-27 11:22:38 +0200

    preloadImages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,64 @@
 import { useState, useEffect } from "react";
-import LandingPage from "./pages/LandingPage";
 import CycleOne from "./pages/CycleOne";
 import CycleTwo from "./pages/CycleTwo";
 import CycleThree from "./pages/CycleThree";
 import CycleFour from "./pages/CycleFour";
 import FinalPage from "./pages/FinalPage";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
+  const [imagesLoaded, setImagesLoaded] = useState(false); // Ajouter un état pour suivre le chargement des images
   const [activePageIndex, setActivePageIndex] = useState(0);
 
   const pages = [
     { component: <LandingPage />, duration: 1800 },
     { component: <CycleOne />, duration: 3000 },
     { component: <CycleTwo />, duration: 3600 },
-    { component: <CycleThree />, duration: 4300 },
-    { component: <CycleFour />, duration: 3800 },
+    { component: <CycleThree />, duration: 4500 },
+    { component: <CycleFour />, duration: 3900 },
     { component: <FinalPage />, duration: 3000 },
   ];
 
+  // Liste des URLs des images utilisées dans les pages
+  const imageUrls = [
+    "./assets/images/josh.webp",
+    "./assets/images/josh1.webp",
+    "./assets/images/josh2.webp",
+    "./assets/images/josh3.webp",
+    "./assets/images/josh4.webp",
+    "./assets/images/josh5.webp",
+  ];
+
   useEffect(() => {
+    // Fonction pour précharger les images
+    const preloadImages = async () => {
+      try {
+        await Promise.all(
+          imageUrls.map((url) => {
+            return new Promise((resolve, reject) => {
+              const img = new Image();
+              img.src = url;
+              img.onload = resolve;
+              img.onerror = reject;
+            });
+          })
+        );
+        // Toutes les images sont chargées, on peut afficher l'application
+        setImagesLoaded(true);
+      } catch (error) {
+        console.error("Erreur lors du chargement des images:", error);
+        // En cas d'erreur, on peut toujours afficher l'application, mais les images pourraient ne pas être fluides
+        setImagesLoaded(true);
+      }
+    };
+
+    preloadImages();
+  }, []); // Utilisez une dépendance vide pour que cela ne soit exécuté qu'une seule fois lors du chargement initial
+
+  useEffect(() => {
+    // Ne pas exécuter l'intervalle tant que les images ne sont pas préchargées
+    if (!imagesLoaded) return;
+
     const interval = setInterval(() => {
       setActivePageIndex((prevIndex) => {
         // Si c'est la dernière page, arrêter l'intervalle
@@ -33,7 +73,11 @@ function App() {
     }, pages[activePageIndex].duration);
 
     return () => clearInterval(interval);
-  }, [activePageIndex, pages]);
+  }, [activePageIndex, pages, imagesLoaded]); // Ajouter imagesLoaded comme dépendance ici
+
+  if (!imagesLoaded) {
+    return <div>Chargement...</div>; // Affichez un écran de chargement jusqu'à ce que les images soient préchargées
+  }
 
   return (
     <div className="appcontainer">
