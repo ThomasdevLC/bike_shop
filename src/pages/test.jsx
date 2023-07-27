e8b9db39b9c974b08fbf85b14ae4b6ad29e6d84e
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-27 12:22:41 +0200

    test

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,60 +1,45 @@
 import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
 import CycleOne from "./pages/CycleOne";
 import CycleTwo from "./pages/CycleTwo";
 import CycleThree from "./pages/CycleThree";
 import CycleFour from "./pages/CycleFour";
 import FinalPage from "./pages/FinalPage";
+import LandingPage from "./pages/LandingPage";
 
-function test() {
-  const [currentPage, setCurrentPage] = useState(1);
+function App() {
+  const [activePageIndex, setActivePageIndex] = useState(0);
+
+  const pages = [
+    { component: <LandingPage />, duration: 1800 },
+    { component: <CycleOne />, duration: 3000 },
+    { component: <CycleTwo />, duration: 3600 },
+    { component: <CycleThree />, duration: 4500 },
+    { component: <CycleFour />, duration: 3900 },
+    { component: <FinalPage />, duration: 3000 },
+  ];
 
   useEffect(() => {
-    // Changer de page automatiquement toutes les 3 secondes
-    const pageInterval = setInterval(() => {
-      setCurrentPage((prevPage) => prevPage + 1);
-    }, 4100);
+    const interval = setInterval(() => {
+      setActivePageIndex((prevIndex) => {
+        // Si c'est la dernière page, arrêter l'intervalle
+        if (prevIndex === pages.length - 1) {
+          clearInterval(interval);
+        }
+        // Passer à la page suivante, sauf si c'est la dernière page
+        return prevIndex === pages.length - 1
+          ? prevIndex
+          : (prevIndex + 1) % pages.length;
+      });
+    }, pages[activePageIndex].duration);
 
-    // Nettoyer l'intervalle lorsque le composant est démonté
-    return () => clearInterval(pageInterval);
-  }, []);
-
-  // Définir ici l'enchaînement des pages selon la variable 'currentPage'
-  let pageToRender;
-  switch (currentPage) {
-    case 1:
-      pageToRender = <CycleOne />;
-      break;
-    case 2:
-      pageToRender = <CycleTwo />;
-      break;
-    case 3:
-      pageToRender = <CycleThree />;
-      break;
-    case 4:
-      pageToRender = <CycleFour />;
-      break;
-    case 5:
-      pageToRender = <FinalPage />;
-      break;
-    default:
-      pageToRender = null;
-  }
+    return () => clearInterval(interval);
+  }, [activePageIndex, pages]); // Ajouter imagesLoaded comme dépendance ici
 
   return (
     <div className="appcontainer">
-      <motion.div
-        className="appcontainer__page"
-        key={currentPage}
-        initial={{ opacity: 0, y: "-100%" }}
-        animate={{ opacity: 1, y: "0%" }}
-        exit={{ opacity: 0, y: "100%" }}
-        transition={{ duration: 0.5 }}
-      >
-        {pageToRender}
-      </motion.div>
+      <div>{pages[activePageIndex].component}</div>
     </div>
   );
 }
 
-export default test;
+export default App;
