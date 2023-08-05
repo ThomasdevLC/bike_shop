b9b4b2ee9133caf68a01c668bd7661511e89548d
ThomasdevLC <thomas.lecam@gmail.com>
2023-08-05 11:02:24 +0200

    thankspage image + text

--- src/pages/ThanksPage.jsx
+++ src/pages/ThanksPage.jsx
@@ -1,9 +1,13 @@
+import rider from "../assets/images/rider.jpg";
 const ThanksPage = () => {
   return (
     <div className="thankspage">
       <p className="thankspage__text">
         You can <br /> ride now
       </p>
+      <div className="thankspage__image">
+        <img src={rider} alt="rider picture" />
+      </div>
     </div>
   );
 };
