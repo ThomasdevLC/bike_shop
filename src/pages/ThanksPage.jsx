01e005f4352468a59b5afc5b16b3870adf558c18
ThomasdevLC <thomas.lecam@gmail.com>
2023-08-05 14:32:52 +0200

    thanskpage style

--- src/pages/ThanksPage.jsx
+++ src/pages/ThanksPage.jsx
@@ -4,27 +4,21 @@ const ThanksPage = () => {
   const textTopRef = useRef(null);
   const textBottomRef = useRef(null);
   const spanTopRef = useRef(null);
-  const boxContainerRef = useRef(null);
   const boxRiderRef = useRef(null);
 
   useEffect(() => {
     const top = textTopRef.current;
     const bottom = textBottomRef.current;
     const span = spanTopRef.current;
-    const riderContainer = boxContainerRef.current;
     const rider = boxRiderRef.current;
 
-    gsap.to([riderContainer], {
-      rotate: 6,
-      ease: "power3.out",
-    });
     gsap.fromTo(
       [top, bottom, span],
       { y: "110%" },
       { y: 0, stagger: 0.4, duration: 0.6 }
     );
     gsap.fromTo(
-      [rider],
+      rider,
       { scaleY: 1.5 },
       {
         scaleY: 0,
@@ -48,7 +42,7 @@ const ThanksPage = () => {
           </p>
         </div>
       </div>
-      <div className="thankspage__box" ref={boxContainerRef}>
+      <div className="thankspage__box">
         <div className="thankspage__box__slide" ref={boxRiderRef}></div>
         <div className="thankspage__box__slide__image">
           <img src={rider} alt="rider picture" />
