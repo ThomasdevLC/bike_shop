09c18cf5179f0661a954513909a32083cd3cd8b3
ThomasdevLC <thomas.lecam@gmail.com>
2023-09-14 16:55:13 +0200

    navigate + brand

--- src/pages/ThanksPage.jsx
+++ src/pages/ThanksPage.jsx
@@ -1,10 +1,18 @@
 import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
 import rider from "../assets/images/rider.jpg";
 const ThanksPage = () => {
+  const navigate = useNavigate();
+
   const textTopRef = useRef(null);
   const textBottomRef = useRef(null);
   const spanTopRef = useRef(null);
   const boxRiderRef = useRef(null);
+
+  const handleShopClick = () => {
+    navigate("/Shop");
+  };
 
   useEffect(() => {
     const top = textTopRef.current;
@@ -12,16 +20,8 @@ const ThanksPage = () => {
     const span = spanTopRef.current;
     const rider = boxRiderRef.current;
 
-    gsap.fromTo(
-      [top, bottom, span],
-      { y: "110%" },
-      { y: 0, stagger: 0.4, duration: 0.6 }
-    );
-    gsap.fromTo(
-      [top, bottom, span],
-      { y: "110%" },
-      { y: 0, stagger: 0.4, duration: 0.6 }
-    );
+    gsap.fromTo([top, bottom, span], { y: "110%" }, { y: 0, stagger: 0.4, duration: 0.6 });
+    gsap.fromTo([top, bottom, span], { y: "110%" }, { y: 0, stagger: 0.4, duration: 0.6 });
     gsap.fromTo(
       rider,
       { scaleY: 1.5 },
@@ -36,6 +36,13 @@ const ThanksPage = () => {
 
   return (
     <div className="thankspage">
+      <div className="thankspage__top">
+        <p className="thankspage__top__brand">ASPHALT</p>
+        <p className="thankspage__top__back" onClick={handleShopClick}>
+          <span className="material-symbols-outlined thankspage__top__back__arrow ">arrow_back_ios</span>
+          back to shop
+        </p>
+      </div>
       <div className="thankspage__text">
         <div className="thankspage__text__top">
           <p className="thankspage__text__top__txt" ref={textTopRef}>
